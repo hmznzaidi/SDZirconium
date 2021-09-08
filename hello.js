83246ece0edf737992e9a64899d5299155c39dcd
L I T O <danielyusoff08@gmail.com>
2021-09-08 15:38:38 +0800

    Complete Overhaul

--- hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const curry = f => (...args) => args.length >= f.length ? f(...args) : curry(f.bind(f, ...args));
-
-const compose = (f, g) => x => f(g(x));
-
-const composeN = (...fns) => (...args) => fns.reverse().reduce((x, f) => f.apply(f, [].concat(x)), args);
-
-setTimeout(function(){window.location='home.php';},5000);
--- logout.php
+++ logout.php
@@ -1,15 +1,12 @@
 <?php
-session_start(); 
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 60*60,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-unset($_SESSION['login']);
-session_destroy(); // destroy session
-header("location:index.php"); 
-?>
 
+session_start();
+
+if(isset($_SESSION['user_id']))
+{
+	unset($_SESSION['user_id']);
+
+}
+
+header("Location: login.php");
+die;
