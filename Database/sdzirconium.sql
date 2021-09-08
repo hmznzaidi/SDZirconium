b2763c7df31a0ee1846a4ea8bde13d2b611c4ad3
mhmnal <mhmn.inabox@gmail.com>
2021-09-08 16:11:56 +0800

    updated database

---
+++ Database/sdzirconium.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 08, 2021 at 10:11 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sdzirconium`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `first_name` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `city` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `gender` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_id`, `user_name`, `last_name`, `password`, `date`, `first_name`, `country`, `city`, `gender`) VALUES
+(1, 41803431668, 'Lito08', '', 'Dy081201', '2021-09-08 06:20:02', '', '', '', ''),
+(2, 6246852455, 'danielyusoff08', '', 'Dy081201', '2021-09-08 06:21:34', '', '', '', ''),
+(3, 7145409035, 'shaheen', '', 'Dy081201', '2021-09-08 07:06:07', '', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `date` (`date`),
+  ADD KEY `user_name` (`user_name`),
+  ADD KEY `last_name` (`last_name`),
+  ADD KEY `first_name` (`first_name`),
+  ADD KEY `country` (`country`),
+  ADD KEY `city` (`city`),
+  ADD KEY `gender` (`gender`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
